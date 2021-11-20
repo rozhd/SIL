cd1e25a23e43c0eca42aa3d3e0e93683b844eed9
rozhd <rozhd>
2021-11-20 06:38:50 +0300

    randomizer ramdomize!

---
+++ main.js
@@ -0,0 +1,9 @@
+var textArray=['Design an interface that allows gym trainers to create a workout plan for their clients and track their progress.',
+               'Design an interface for securely finding a roommate/apartment in your city.',
+               'Design an interface for notes widget.']
+function randomTask() {
+    var value = textArray.shift(); //Get the first item from the array
+    textArray.push(value); //Push it back for the cycle to repeat.
+    document.getElementById("task_text").innerHTML=value + ' Don’t&nbsp;forget to&nbsp;upload your interface to&nbsp;your portfolio.';
+    value = null();
+}
