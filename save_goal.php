e8dd72092533d5fda28bc32e205a70cb24dcca98
rozhd <rozhd>
2021-11-29 17:47:44 +0300

    added save_goal_php

---
+++ save_goal.php
@@ -0,0 +1,15 @@
+<?php 
+
+$date = date("Y-M-D H:i:s");
+
+$browser = $_SERVER["HTTP_USER_AGENT"];
+
+$ip = $_SERVER["REMOTE_ADDR"];
+
+$s = "$date,$browser,$ip\n";
+
+file_put_contents("goals.csv", $s, FILE_APPEND);
+
+
+
+?>
